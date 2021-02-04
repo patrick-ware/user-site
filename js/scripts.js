10fac0b26433cca632c444e78df4cc54728a7e4f
Patrick Ware <patrick.r.ware@gmail.com>
2021-02-04 01:26:05 -0500

    preloader integration

--- js/scripts.js
+++ js/scripts.js
@@ -60,4 +60,13 @@
 //    }
 //  });
 
+  $(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+ 
+  });
+
 })(jQuery); // End of use strict
